3c34950d157480e3a7942b9b472a95ddd3c3699b
dolsen0 <derek.olsen@gmail.com>
2023-02-27 22:53:28 -0500

    updated cors

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
+const cors = require('cors');
 const generateRandomPrompt = require('./generator');
 
 admin.initializeApp();
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Game Generator');
@@ -17,5 +20,3 @@ app.get('/main', (req, res) => {
 });
 
 exports.api = functions.https.onRequest(app);
-
-//https://gamegenerator-cc1ec.web.app/main
